678fc1a2fdea17984ef247d3db949d88a4d75337
MH_Guo <guominhuan@jssvc.edu.cn>
2020-11-03 16:17:23 +0800

    rebuild 001,002,003

--- 003_opencv_test_Bar_Click/src/test.cpp
+++ 003_opencv_test_Bar_Click/src/test.cpp
@@ -5,10 +5,18 @@ using namespace std;
 
 // OpenCV includes
 #include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 using namespace cv;
 
-const int CV_GUI_NORMAL= 0x10;
+// Create a variable to save the position value in track
+int blurAmount=15;
+
+// Trackbar call back function
+static void onChange(int pos, void* userInput);
+
+//Mouse callback
+static void onMouse( int event, int x, int y, int, void* userInput );
 
 int main( int argc, const char** argv )
 {
@@ -18,26 +26,54 @@ int main( int argc, const char** argv )
 	// Create windows
 	namedWindow("Lena");
 	
-	// show images
-	imshow("Lena", lena);
+	// create a trackbark
+	createTrackbar("Lena", "Lena", &blurAmount, 30, onChange, &lena);
 	
-	// Display Overlay
-	displayOverlay("Lena", "Overlay 5secs", 5000);
+	setMouseCallback("Lena", onMouse, &lena);
 
-	// Display Status Bar
-	displayStatusBar("Lena", "Status bar 5secs", 5000);
-
-	// Save window parameters
-	saveWindowParameters("Lena");
-
-	// load Window parameters
-	loadWindowParameters("Lena");
-
-	// wait for any key press
+	// Call to onChange to init
+	onChange(blurAmount, &lena);
+		
+	// wait app for a key to exit
 	waitKey(0);
-
+	
 	// Destroy the windows
 	destroyWindow("Lena");
 	
 	return 0;
+}
+
+// Trackbar call back function
+static void onChange(int pos, void* userInput)
+{
+	if(pos <= 0)
+		return;
+	// Aux variable for result
+	Mat imgBlur;
+
+	// Get the pointer input image
+	Mat* img= (Mat*)userInput;
+
+	// Apply a blur filter
+	blur(*img, imgBlur, Size(pos, pos));	
+
+	// Show the result
+	imshow("Lena", imgBlur);
+}
+
+//Mouse callback
+static void onMouse( int event, int x, int y, int, void* userInput )
+{
+	if( event != EVENT_LBUTTONDOWN )
+	        return;
+
+	// Get the pointer input image
+	Mat* img= (Mat*)userInput;
+	
+	// Draw circle
+	circle(*img, Point(x, y), 10, Scalar(0,255,0), 3);
+
+	// Call on change to get blurred image
+	onChange(blurAmount, img);
+
 }
